1d984d365a26cc1644c57afbb6fc53db38c098b6
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-04-30 15:45:47 +0500

    Create CurrencyRatesDbModel.kt that used for storing the currency rates in db.

---
+++ app/src/main/java/com/asad/currencyconverter/data/local/models/CurrencyRatesDbModel.kt
@@ -0,0 +1,12 @@
+package com.asad.currencyconverter.data.local.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.asad.currencyconverter.utils.Constants
+
+@Entity(tableName = Constants.CURRENCY_RATES_TABLE)
+data class CurrencyRatesDbModel constructor(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    var currencyName: String,
+    var currencyRate: Double
+)
