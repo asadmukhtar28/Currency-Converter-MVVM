c87f64f9496d132771baedd00952697f3b33814c
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-04-30 18:05:37 +0500

    Write a function to perform conversion b/w selected currencies at CurrencyRatesViewModel.kt.

--- app/src/main/java/com/asad/currencyconverter/ui/currencyrates/CurrencyRatesViewModel.kt
+++ app/src/main/java/com/asad/currencyconverter/ui/currencyrates/CurrencyRatesViewModel.kt
@@ -1,10 +1,12 @@
 package com.asad.currencyconverter.ui.currencyrates
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.asad.currencyconverter.CurrencyConverterApp
 import com.asad.currencyconverter.data.local.models.CurrencyRatesDbModel
-import com.asad.currencyconverter.ui.base.BaseViewModel
+import com.asad.currencyconverter.utils.convertDoubleTo2Decimal
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -12,7 +14,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CurrencyRatesViewModel @Inject constructor(private val repository: CurrencyRatesRepository) :
-    BaseViewModel(repository.dataManager) {
+    ViewModel() {
 
     private val _uiState = MutableStateFlow<UiStates>(LoadingState)
     val uiState: StateFlow<UiStates> = _uiState
@@ -20,6 +22,8 @@ class CurrencyRatesViewModel @Inject constructor(private val repository: Currenc
     private val _currencyRatesList = MutableStateFlow<List<CurrencyRatesDbModel>>(arrayListOf())
     val currencyRatesList: StateFlow<List<CurrencyRatesDbModel>> = _currencyRatesList
 
+    private val _selectedCurrency = MutableStateFlow(CurrencyRatesDbModel())
+    val selectedCurrency: StateFlow<CurrencyRatesDbModel> = _selectedCurrency
 
     init {
         fetchCurrencySymbols()
@@ -28,12 +32,45 @@ class CurrencyRatesViewModel @Inject constructor(private val repository: Currenc
     private fun fetchCurrencySymbols() = viewModelScope.launch {
         repository.fetchCurrencyRatesFromDb().collect {
             if (it.isNotEmpty()) {
-                delay(1000)
                 _uiState.value = ContentState
                 _currencyRatesList.value = it
+                _selectedCurrency.value = _currencyRatesList.value.first()
             } else {
-                _uiState.value = ErrorState
+                if (CurrencyConverterApp.isNetworkConnected) {
+                    _uiState.value = LoadingState
+                } else _uiState.value = ErrorState
             }
         }
+    }
+
+    fun performConversion(amount: Double) {
+        viewModelScope.launch(Dispatchers.Default) {
+            val list: MutableList<CurrencyRatesDbModel> = ArrayList()
+            currencyRatesList.value.let { listItems ->
+                if (listItems.isNotEmpty()) {
+                    val currencyValue = amount / listItems.filter {
+                        selectedCurrency.value.currencyName == it.currencyName
+                    }.map {
+                        it.currencyRate
+                    }.first()
+
+                    listItems.forEach {
+                        list.add(
+                            CurrencyRatesDbModel(
+                                currencyName = it.currencyName,
+                                currencyRate = (it.currencyRate * currencyValue).convertDoubleTo2Decimal()
+                            )
+                        )
+                    }
+                }
+            }
+
+            _currencyRatesList.value = list
+        }
+
+    }
+
+    fun updateSelectedCurrency(currencyRatesModel: CurrencyRatesDbModel) {
+        _selectedCurrency.value = currencyRatesModel
     }
 }
