670c064b888586a4e1ee13ddc9e2a85bf91ee03f
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-04-30 15:41:39 +0500

    Inject the dbhelper at AppDataManager.kt, AppDataManagerImpl.kt to use the single point for api service and db operations.

--- app/src/main/java/com/asad/currencyconverter/data/manager/AppDataManager.kt
+++ app/src/main/java/com/asad/currencyconverter/data/manager/AppDataManager.kt
@@ -1,7 +1,9 @@
 package com.asad.currencyconverter.data.manager
 
+import com.asad.currencyconverter.data.local.helper.CurrencyAppDbHelper
 import com.asad.currencyconverter.data.remote.api.CurrencyExchangeApi
 
 interface AppDataManager {
     fun getApiHelper(): CurrencyExchangeApi
+    fun getAppDatabaseHelper(): CurrencyAppDbHelper
 }
--- app/src/main/java/com/asad/currencyconverter/data/manager/AppDataManagerImpl.kt
+++ app/src/main/java/com/asad/currencyconverter/data/manager/AppDataManagerImpl.kt
@@ -1,13 +1,19 @@
 package com.asad.currencyconverter.data.manager
 
+import com.asad.currencyconverter.data.local.helper.CurrencyAppDbHelper
 import com.asad.currencyconverter.data.remote.api.CurrencyExchangeApi
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AppDataManagerImpl @Inject constructor(private val apiService: CurrencyExchangeApi) :
-    AppDataManager {
-    override fun getApiHelper(): CurrencyExchangeApi {
-        return apiService
+class AppDataManagerImpl @Inject constructor(
+    val apiService: CurrencyExchangeApi,
+    val appDbHelper: CurrencyAppDbHelper
+) : AppDataManager {
+    override fun getApiHelper() = apiService
+
+    override fun getAppDatabaseHelper(): CurrencyAppDbHelper {
+        return appDbHelper
     }
+
 }
