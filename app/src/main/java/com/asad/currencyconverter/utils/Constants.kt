b6715737c4772b5581221a130758d2eb3e721e96
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-04-30 15:38:48 +0500

    Define Constants.kt object that contains constants for DB tables and Db name.

---
+++ app/src/main/java/com/asad/currencyconverter/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.asad.currencyconverter.utils
+
+object Constants {
+    const val CURRENCY_APP_DB_NAME = "currency_exchange.db"
+    const val CURRENCY_SYMBOL_TABLE = "CURRENCY_SYMBOL_TABLE"
+    const val CURRENCY_RATES_TABLE = "CURRENCY_RATES_TABLE"
+}
